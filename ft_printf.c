f2dfcebf48b0bcda11126b96ef1d7f56398f4161
kalli010 <elkalaizakariae010@gmail.com>
2023-12-12 16:19:33 +0000

    fk left

--- ft_printf.c
+++ ft_printf.c
@@ -34,112 +34,6 @@ int ft_putstr(char *s)
 	return (i);
 }
 
-int ft_putnbr(int num)
-{
-	int digit_count;
-    	int temp;
-
-    	if (num < 0)
-	{
-        	ft_putchar('-');
-        	if (num == -2147483648)
-		{
-            		ft_putnbr(num / 10);
-            		ft_putchar('8');
-            		return 11;
-		}
-        	num = -num;
-    	}
-    	if (num >= 10)
-	{
-        	ft_putnbr(num / 10);
-    	}
-    	ft_putchar(num % 10 + '0');
-    	digit_count = 0;
-    	temp = num;
-    	while (temp > 0)
-	{
-        	digit_count++;
-        	temp /= 10;
-    	}
-    	return digit_count;
-}
-
-
-int ft_putx(unsigned int d, int base)
-{
-	int c;
-	char *s;
-
-	s = "0123456789abcdef";
-	c = 0;
-	if (d < base)
-		c += ft_putchar(s[d]);
-	else
-	{
-		c += ft_putx(d / base, base);
-		c += ft_putchar(s[d % base]);
-	}
-	return (c);
-}
-
-int ft_putX(int d, int base)
-{
-	int c;
-	char *s;
-
-	s = "0123456789ABCDEF";
-	c = 0;
-	if (d < base)
-		return (ft_putchar(s[d]));
-	else
-	{
-		c = ft_putX(d / base, base);
-		return (c + ft_putchar(s[d % base]));
-	}
-}
-
-int ft_putpointer(unsigned long long num)
-{
-    	char hex_string[16];
-    	char *hex_digits = "0123456789abcdef";
-    	int index;
-	int count;
-	int i;
-	
-	index = 15;
-	count = 0;
-    	hex_string[index--] = '\0';
-	if (num == 0)
-    	{
-		hex_string[index] = '0';
-    	}
-    	while (num > 0)
-	{
-        	hex_string[index--] = hex_digits[num % 16];
-        	num /= 16;
-    	}
-    	count += ft_putchar('0');
-    	count += ft_putchar('x');
-    	i = index + 1;
-	while (hex_string[i] != '\0')
-	{
-        	count += ft_putchar(hex_string[i]);
-    		i++;
-	}
-    	return count;
-}
-
-int ft_putunsigned(unsigned int n) {
-   	 int count = 0;
-
-    	if (n / 10 != 0)
-    	{
-        	count += ft_putunsigned(n / 10);
-    	}
-    	count += ft_putchar(n % 10 + '0'); 
-    	return count;
-}
 
 int	my_printf(const char *format, va_list ap)
 {
@@ -167,7 +61,7 @@ int	my_printf(const char *format, va_list ap)
 	return (c);
 }
 
-int	printf( const char *restrict format, ... )
+int	ft_printf(const char *format, ...)
 {
 	va_list ap;
 	int c;
